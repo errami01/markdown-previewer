c81a3411231add99fa85e1d0e0a567a93aceb470
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-03-26 11:50:02 +0000

    Style: set preview and editor height

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 
 import './App.css';
-
+import React, {useState, useRef } from 'react'
+// import { marked } from 'marked'; 
 function App() {
+  const [text, setText] = useState("")
+  const html = useRef("")
+  function handleTextChange(event){
+    const input =event.target.value 
+    setText(input)
+    html.current = window.marked.parse(input)
+
+  }
   return (
-    <div className="App">
-     
+    <div className="App" >
+      <textarea id="editor" onChange={handleTextChange} value={text} />
+      <div id="preview" dangerouslySetInnerHTML={{ __html: html.current }}/>
     </div>
   );
 }

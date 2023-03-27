ba8fb5cc702de0b57af7e6da739d79484242752d
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-03-27 06:03:26 +0000

    install reat-markdown

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import ReactMarkdown from "react-markdown";
 
 function App() {
   return (
     <div className="App">
-     
+     <ReactMarkdown># Hello</ReactMarkdown>
+     <ReactMarkdown>## Hello</ReactMarkdown>
+     <ReactMarkdown>### Hello</ReactMarkdown>
     </div>
   );
 }

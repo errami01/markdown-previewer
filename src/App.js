81eb333c586fee64ec31a00d92b0c4d0c88e59c7
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-03-27 06:08:31 +0000

    install remark-gfm plugin

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 
 import './App.css';
 import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
 function App() {
   return (
     <div className="App">
-     <ReactMarkdown># Hello</ReactMarkdown>
-     <ReactMarkdown>## Hello</ReactMarkdown>
-     <ReactMarkdown>### Hello</ReactMarkdown>
+     <ReactMarkdown>**A bold text**</ReactMarkdown>
+<ReactMarkdown>*An italic text*</ReactMarkdown>
+<ReactMarkdown>__A strong text__</ReactMarkdown>
+<ReactMarkdown>1. An ordered list text</ReactMarkdown>
+<ReactMarkdown>- An unordered list text</ReactMarkdown>
+<ReactMarkdown> ~~A strikethrough text~~ </ReactMarkdown>
+<ReactMarkdown remarkPlugins={[remarkGfm]}>
+    ~~A strikethrough text~~
+</ReactMarkdown>
     </div>
   );
 }

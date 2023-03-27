59ebe3727fc7ae2ea6f0404d4399046cc8b77a5e
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-03-27 07:28:49 +0000

    install react-syntax-highlighter

--- src/App.js
+++ src/App.js
@@ -2,19 +2,103 @@
 import './App.css';
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { dark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 function App() {
+  const defaultText = `
+  # Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+### KColjsdjf;aldjasdj
+Heres some code, \~ <div></div>\~, between 2 backticks.
+
+~~~javascript
+// this is multi-line code:kj
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '~~~
+  ' && lastLine == '~~~
+  ') {
+    return multiLineCode;
+  }
+}
+~~~
+
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.org), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+
+  `;
+  const javaScriptMarkdownCodeblock = `JavaScript code example:
+ 
+~~~javascript
+// function that adds "2 numbers" together
+const sumTwoNumbers = (num1, num2) => num1 + num2;
+ 
+// call the function
+console.log(sumTwoNumbers(1, 2)); // 3
+ 
+// array of users
+const users = [
+  { name: "John", age: 30 },
+  { name: "Jane", age: 28 },
+  { name: "Bob", age: 25 },
+];
+ 
+// print out the users age 
+console.log(users.map(user => user.age)); // [30, 28, 25]
+~~~
+`;
   return (
     <div className="App">
-     <ReactMarkdown>**A bold text**</ReactMarkdown>
-<ReactMarkdown>*An italic text*</ReactMarkdown>
-<ReactMarkdown>__A strong text__</ReactMarkdown>
-<ReactMarkdown>1. An ordered list text</ReactMarkdown>
-<ReactMarkdown>- An unordered list text</ReactMarkdown>
-<ReactMarkdown> ~~A strikethrough text~~ </ReactMarkdown>
-<ReactMarkdown remarkPlugins={[remarkGfm]}>
-    ~~A strikethrough text~~
-</ReactMarkdown>
+     <ReactMarkdown
+     remarkPlugins={[remarkGfm]}
+  children={defaultText}
+  components={{
+    code({ node, inline, className, children, ...props }) {
+      const match = /language-(\w+)/.exec(className || "");
+      return !inline && match ? (
+        <SyntaxHighlighter
+          children={String(children).replace(/\n$/, "")}
+          style={dark} // theme
+          language={match[1]}
+          PreTag='section' // parent tag
+          {...props}
+        />
+      ) : (
+        <code className={className} {...props}>
+          {children}
+        </code>
+      );
+    },
+  }}
+/>
     </div>
   );
 }
